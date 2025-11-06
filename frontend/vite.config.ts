0cd3116db5ac08765ce5013abed6766e84ed4cbf
Johnny Zhu <zhu.johnny0@gmail.com>
2025-11-06 01:14:30 -0500

    package stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
   ],
   server: {
     port: 5173,
-    proxy: { "/video-feed": "http://localhost:8000" }
+    proxy: {
+      "/video-feed": "http://localhost:8000",
+      "/session": "http://localhost:8000",
+      "/update-location": "http://localhost:8000",
+      "/current-location": "http://localhost:8000",
+      "/health": "http://localhost:8000",
+      "/events": "http://localhost:8000",
+      "/last-dets": "http://localhost:8000"
+    }
   }
 })
